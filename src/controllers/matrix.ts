5598dc1df6bf6820a00033c2498afcd042087f02
Matan Amir <matan6080@gmail.com>
2020-11-18 00:59:05 +0200

    imrovements

--- src/controllers/matrix.ts
+++ src/controllers/matrix.ts
@@ -1,4 +1,6 @@
 import { Request, Response } from 'express';
+import fs from 'fs';
+import path from 'path';
 
 export const generateRandomMatrix = (req: Request, res: Response) => {
   res.send('generate matrix');
@@ -6,7 +8,12 @@ export const generateRandomMatrix = (req: Request, res: Response) => {
 
 export const uploadFormultiplication = (req: Request, res: Response) => {
   console.log(req.file);
-  res.send('uploadFormultiplication');
+  const writeStream = fs.createWriteStream(
+    path.join(__dirname, '..', 'uploads', req.file.originalname),
+    { flags: 'w' }
+    );
+  writeStream.write(req.file.buffer.toString());
+  writeStream.end(());
 };
 
 export const getMultiplicationResult = (req: Request, res: Response) => {
--- src/routes/matrix.ts
+++ src/routes/matrix.ts
@@ -1,16 +1,16 @@
 import express from 'express';
+import multer from 'multer';
 import {
   generateRandomMatrix,
   getMultiplicationResult,
   uploadFormultiplication,
 } from '../controllers/matrix';
-import { uploadStorgae } from '../fileStrorage';
 
 const router = express.Router();
 
 // TODO add express validator for checking...
 router.post('/generate', generateRandomMatrix);
-router.post('/upload', uploadStorgae.single('file'), uploadFormultiplication);
+router.post('/upload', multer().single('file'), uploadFormultiplication);
 router.get('/multiplication', getMultiplicationResult);
 
 export { router as matrixRouter };
