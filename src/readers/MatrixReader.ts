1b7832bd7d1cebd686d5b6a078b9cb9eb317efb5
Matan Amir <matan6080@gmail.com>
2020-11-17 16:42:14 +0200

    Simple csvfilereader and matrixfilereader

--- src/readers/MatrixReader.ts
+++ src/readers/MatrixReader.ts
@@ -1,19 +1,29 @@
-import fs from 'fs';
+import { CsvFileReader } from './CsvFileReader';
 import { DataReader } from '../interfaces/DataReader';
 
-export class MatrixReader implements DataReader {
-  data: string[][] = [];
+export class MatrixReader {
+  static fromCsv(filename: string): MatrixReader {
+    return new MatrixReader(new CsvFileReader(filename));
+  }
 
-  constructor(public filename: string) {}
+  //   matches: MatchData[] = [];
 
-  read(): void {
-    this.data = fs
-      .readFileSync(this.filename, {
-        encoding: 'utf-8',
-      })
-      .split('\n')
-      .map((row: string): string[] => {
-        return row.split(',');
-      });
+  constructor(public reader: DataReader) {}
+
+  load(): void {
+    this.reader.read();
+    // this.matches = this.reader.data.map(
+    //   (row: string[]): MatchData => {
+    //     return [
+    //       dateStringToDate(row[0]),
+    //       row[1],
+    //       row[2],
+    //       parseInt(row[3]),
+    //       parseInt(row[4]),
+    //       row[5] as MatchResult,
+    //       row[6],
+    //     ];
+    //   }
+    // );
   }
 }
