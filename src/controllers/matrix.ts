d52ddbe3ad4b63e0f467de3237a0e13bbaa8b41f
Matan Amir <matan6080@gmail.com>
2020-11-18 21:06:40 +0200

    result of matrices

--- src/controllers/matrix.ts
+++ src/controllers/matrix.ts
@@ -35,13 +35,31 @@ export const getMultiplicationResult = async (req: Request, res: Response) => {
     const matrixReader2 = MatrixReader.fromCsv(
       path.join(__dirname, '..', 'uploads', 'matrix2.txt')
     );
-    await matrixReader1.load();
-    await matrixReader2.load();
-    console.log('matrix1', matrixReader1.matrix);
-    console.log('matrxi 2', matrixReader2.matrix);
-    for (let i = 0; i < matrixReader1.matrix.length; i++) {
-      for (let j = 0; j < matrixReader2.matrix.length; j++) {}
+    await Promise.all([matrixReader1.load(), matrixReader2.load()]);
+    // console.log('matrix1', matrixReader1.matrix);
+    // console.log('matrxi 2', matrixReader2.matrix);
+    const { matrix: matrix1 } = matrixReader1;
+    const { matrix: matrix2 } = matrixReader2;
+    const results = [],
+      ITERATION_NUMBER = matrix1.length * 2;
+    let sum = 0,
+      k = 0;
+    for (let i = 0; i < ITERATION_NUMBER; i++) {
+      for (let j = 0; j < matrix2.length; j++) {
+        let index = i;
+        if (i >= matrix1.length) {
+          index = i - matrix1.length;
+        }
+        sum += matrix1[index][j] * matrix2[j][k];
+      }
+      if (i + 1 === matrix1.length) {
+        // * if this is equal we need to start new iteration
+        k++;
+      }
+      results.push(sum);
+      sum = 0;
     }
+    console.log('result', results);
   } catch (error) {
     throw new Error(`Cannot calcaulte marices: ${error.message}`);
   }
